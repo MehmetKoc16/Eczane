8938f41249f6a2973dbf9c0c8765ea6149f649f2
MehmetKoç <mehmet.koc.brs1453@gmail.com>
2025-06-02 17:48:27 +0300

    eczane_service yazıldı.

---
+++ lib/Service/eczane_service.dart
@@ -0,0 +1,104 @@
+import 'package:dio/dio.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:eczane/Model/eczane_model.dart';
+
+class EczaneService {
+  static const String _baseUrl =
+      "https://api.collectapi.com/health/dutyPharmacy";
+  static const String _apikey =
+      "apikey 1oqGF0Gn8lifA0WlvIUDW2:2LTFiBDHtghi3tSrqdceJy";
+
+  final Dio _dio = Dio();
+  Future<Map<String, String>> _getLocation() async {
+    final bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception("Konum servisi kapalı. Lütfen açınız.");
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception("Konum izni reddedildi. Lütfen izin veriniz.");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+          "Konum izni kalıcı olarak reddedildi.Uygulama ayarlarından izin vermelisiniz.");
+    }
+    final Position position = await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 10,
+      ),
+    );
+    final List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    if (placemarks.isEmpty) {
+      throw Exception("Adres bilgisi alınamadı");
+    }
+    final String city = placemarks[0].administrativeArea ?? '';
+    final String district = placemarks[0].subAdministrativeArea ?? '';
+    if (city.isEmpty) {
+      throw Exception("Şehir bilgisi alınamadı.");
+    }
+
+    return {'city': city, 'district': district};
+  }
+
+  Future<List<EczaneModel>> getOnDutyEczane(
+      {String? city, String? district}) async {
+    String targetCity = city ?? '';
+    String targetDistrict = district ?? '';
+    try {
+      if (targetCity.isEmpty || targetDistrict.isEmpty) {
+        final Map<String, String> locationData = await _getLocation();
+        targetCity = locationData['city'] ?? '';
+        targetDistrict = locationData['district'] ?? '';
+        if (targetCity.isEmpty) {
+          throw Exception("Konum bilgisi (şehir) alınamadı veya belirtilmedi.");
+        }
+      }
+      String url = '$_baseUrl?il=${Uri.encodeComponent(targetCity)}';
+      if (targetDistrict.isNotEmpty) {
+        url += '&ilce=${Uri.encodeComponent(targetDistrict)}';
+      }
+      final Map<String, dynamic> headers = {
+        "authorization": _apikey,
+        "content-type": "application/json",
+      };
+      final response = await _dio.get(url, options: Options(headers: headers));
+      if (response.statusCode != 200) {
+        throw Exception(
+            "API'den veri çekerken hata oluştu. ${response.statusCode}");
+      }
+      final Map<String, dynamic> responseData = response.data;
+
+      if (responseData['success'] != true) {
+        throw Exception(
+            "API'den başarılı yanıt alınamdı: ${responseData['reason'] ?? 'Bilinmeyen Hata'}");
+      }
+      final List<dynamic> phrmacyResult = responseData["result"];
+      final List<EczaneModel> eczane = phrmacyResult
+          .map((jsonItem) => EczaneModel.fromJson(jsonItem))
+          .toList();
+      return eczane;
+    } on DioException catch (e) {
+      // Dio'ya özgü hataları yakala (ağ hatası, sunucu hatası vb.)
+      if (e.response != null) {
+        print(
+            "Dio Hata Yanıtı: ${e.response?.statusCode} - ${e.response?.data}");
+        throw Exception(
+            "Sunucudan hata yanıtı: ${e.response?.statusCode} - ${e.response?.data['reason'] ?? 'Bilinmeyen Hata'}");
+      } else {
+        print("Dio Hata (İstek): ${e.message}");
+        throw Exception("Ağ hatası veya istek zaman aşımı: ${e.message}");
+      }
+    } catch (e) {
+      // Diğer tüm hataları yakala
+      print("Nöbetçi eczane verileri çekilirken genel bir hata oluştu: $e");
+      rethrow; // Hatanın çağrıya devam etmesini sağla
+    }
+  }
+}
